0d05e88772edfdb4495892b8f116b5b9f6a17668
Falin Pavel <falinpavel96@gmail.com>
2025-07-29 00:15:56 +0300

    added test for download

--- conftest.py
+++ conftest.py
@@ -1,2 +1,19 @@
-import selene
 import pytest
+from selene.support.shared import browser
+from selenium import webdriver
+
+
+@pytest.fixture(scope='function', autouse=True)
+def driver_configuration():
+    options = webdriver.ChromeOptions()
+    options.add_argument('start-maximized')
+    prefs = {
+        "download.default_directory": "tmp",
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True
+    }
+    options.add_experimental_option('prefs', prefs)
+    driver = webdriver.Chrome(options=options)
+    browser.config.driver = driver
+    browser.config.base_url = 'https://www.online-convert.com/ru'
+    return driver
---
+++ tests/test_download_files.py
@@ -0,0 +1,36 @@
+import os
+
+import requests
+from selene import browser, query
+
+from const import TMP_DIR
+
+
+def test_download_file_pdf():
+    browser.open('/file-format/pdf')
+    download_url = browser.element('[title="Скачать example.pdf"]').get(query.attribute("href"))
+    content = requests.get(url=download_url).content
+    assert content is not None
+    with open(TMP_DIR + '/example.pdf', 'wb') as file:
+        file.write(content)
+        assert os.path.exists(TMP_DIR + '/example.pdf')
+
+
+def test_download_file_xlsx():
+    browser.open('/file-format/xlsx')
+    download_url = browser.element('[title="Скачать example.xlsx"]').get(query.attribute("href"))
+    content = requests.get(url=download_url).content
+    assert content is not None
+    with open(TMP_DIR + '/example.xlsx', 'wb') as file:
+        file.write(content)
+        assert os.path.exists(TMP_DIR + '/example.xlsx')
+
+
+def test_download_file_csv():
+    browser.open('/file-format/csv')
+    download_url = browser.element('[title="Скачать example.csv"]').get(query.attribute("href"))
+    content = requests.get(url=download_url).content
+    assert content is not None
+    with open(TMP_DIR + '/example.csv', 'wb') as file:
+        file.write(content)
+        assert os.path.exists(TMP_DIR + '/example.csv')
