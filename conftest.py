99038c35051604da9aed79a865f5cb4c2ba98d29
Falin Pavel <falinpavel96@gmail.com>
2025-07-29 13:16:17 +0300

    edit conftest.py

--- conftest.py
+++ conftest.py
@@ -2,13 +2,15 @@ import pytest
 from selene.support.shared import browser
 from selenium import webdriver
 
+from const import TMP_DIR
+
 
 @pytest.fixture(scope='function', autouse=True)
 def driver_configuration():
     options = webdriver.ChromeOptions()
     options.add_argument('start-maximized')
     prefs = {
-        "download.default_directory": "tmp",
+        "download.default_directory": TMP_DIR,
         "download.prompt_for_download": False,
         "download.directory_upgrade": True
     }
@@ -16,4 +18,5 @@ def driver_configuration():
     driver = webdriver.Chrome(options=options)
     browser.config.driver = driver
     browser.config.base_url = 'https://www.online-convert.com/ru'
-    return driver
+    yield
+    browser.quit()
