06ab491ce8d261384e7468be8f6ab30428b05509
Falin Pavel <falinpavel96@gmail.com>
2025-07-30 17:55:35 +0300

    +2

--- tests/test_archiving_files_to_zip.py
+++ tests/test_archiving_files_to_zip.py
@@ -1,7 +1,9 @@
 import os
+import zipfile
+
 import pandas as pd
 
-from tabulate import tabulate
+# from tabulate import tabulate
 from zipfile import ZipFile
 from pypdf import PdfReader
 from io import BytesIO
@@ -10,10 +12,11 @@ from const import TMP_DIR
 
 def test_archiving_files_to_zip():
     files_to_zip = [f'{TMP_DIR}/example.pdf', f'{TMP_DIR}/example.csv', f'{TMP_DIR}/example.xlsx']
-    with ZipFile(f"{TMP_DIR}/my_homework.zip", 'w') as archive:
+    with ZipFile(f"{TMP_DIR}/my_homework.zip", 'w', allowZip64=True) as archive:
         for file in files_to_zip:
             archive.write(file, arcname=os.path.basename(file))
     assert os.path.exists(f'{TMP_DIR}/my_homework.zip')
+    assert zipfile.is_zipfile(f'{TMP_DIR}/my_homework.zip') is True
 
 
 def test_check_existing_files_in_zip():
