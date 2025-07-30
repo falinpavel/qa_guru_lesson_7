23faa5947845fe24d05f97e7576e0e91dd0a7cb6
Falin Pavel <falinpavel96@gmail.com>
2025-07-30 17:46:25 +0300

    +2

--- tests/test_archiving_files_to_zip.py
+++ tests/test_archiving_files_to_zip.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 
+from tabulate import tabulate
 from zipfile import ZipFile
 from pypdf import PdfReader
 from io import BytesIO
@@ -29,19 +30,26 @@ def test_read_pdf_file_from_zip():
         pdf_file = PdfReader(pdf_stream)
         assert isinstance(pdf_file, PdfReader)
         assert pdf_file.pdf_header == "%PDF-1.5"
-        assert pdf_file.is_encrypted == False
+        assert pdf_file.is_encrypted is False
         assert len(pdf_file.pages) == 1
         assert 'Document file type: PDF' in pdf_file.get_page(0).extract_text()
         assert 'Purpose: Provide example of this file type' in pdf_file.get_page(0).extract_text()
         assert 'File created by http://www.online-convert.com' in pdf_file.get_page(0).extract_text()
-        assert pdf_file.is_encrypted == False
+        assert pdf_file.is_encrypted is False
 
 
 def test_read_csv_file_from_zip():
     with ZipFile(f"{TMP_DIR}/my_homework.zip", mode="r") as archive:
         assert 'example.csv' in archive.namelist()
         csv_stream = BytesIO(archive.read('example.csv'))
-        csv_file = pd.read_csv(csv_stream)
+        csv_file = pd.read_csv(
+            filepath_or_buffer=csv_stream,
+            engine='python',
+            encoding='utf-8',
+            skip_blank_lines=True,
+            keep_default_na=False
+        )
+        # print(tabulate(csv_file, headers='keys', tablefmt='psql', showindex=False))
         assert csv_file.shape == (24, 6)
         assert isinstance(csv_file, pd.DataFrame)
         assert ['CSV test file', 'Unnamed: 1', 'Unnamed: 2',
@@ -51,7 +59,8 @@ def test_read_csv_file_from_zip():
 def test_read_xlsx_file_from_zip():
     with ZipFile(f"{TMP_DIR}/my_homework.zip", mode="r") as archive:
         assert 'example.xlsx' in archive.namelist()
-        xlsx_file = pd.read_excel(archive.read('example.xlsx'), engine='openpyxl')
+        xlsx_stream = BytesIO(archive.read('example.xlsx'))
+        xlsx_file = pd.read_excel(xlsx_stream, engine='openpyxl')
         assert xlsx_file.shape == (16, 7)
         assert isinstance(xlsx_file, pd.DataFrame)
         assert 'XLSX test file' in xlsx_file
